4b94d70d160a69a697ce4033e9d179fbf4ed9488
elizabethLomb <elizabeth.lombardi7@gmail.com>
2020-02-06 17:02:22 +0100

    All configured and ready for start to working

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
